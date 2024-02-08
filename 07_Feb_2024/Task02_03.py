6a01eaa6b391ab1edef1f9022e7a7519981b0c22
Tarun <tarundasgupta9@gmail.com>
2024-02-08 20:08:08 +0530

    Develop a Python script that calculates the square and cube of a given number.

---
+++ 07_Feb_2024/Task02_03.py
@@ -0,0 +1,9 @@
+# Develop a Python script that calculates the square and cube of a given number.
+
+# given no. is 5
+
+n = int(input('Enter the value : '))
+Square = n ** 2
+Cube = n ** 3
+print('\nSquare of the number :', Square)
+print('Cube of the number :', Cube)
