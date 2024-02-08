df9944fadc93de69e94156e6715403bf5752d315
Tarun <tarundasgupta9@gmail.com>
2024-02-08 20:07:31 +0530

    Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.

---
+++ 07_Feb_2024/Task02_02.py
@@ -0,0 +1,20 @@
+# Create a program that takes two numbers as input
+# and prints whether the first number is greater than, less than, or equal to the second number.
+
+# greater than
+X = 10
+Y = 20
+result = (X < Y)
+print(result)
+
+# less than
+X = 30
+Y = 45
+result = (X > Y)
+print(result)
+
+# equal to the second number
+X = 80
+Y = 50
+result = (X == Y)
+print(result)
