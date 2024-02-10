8a27d8d04b7862532dc8cbad265ecf43b4712a68
Tarundasgupta <Tarundasgupta9@gmail.com>
2024-02-10 22:56:17 +0530

    Develop a Python script that calculates the square and cube of a given number.

---
+++ 09_Feb_2024/Task01.py
@@ -0,0 +1,11 @@
+#Create a program that determines whether a given year is a leap year.
+#A leap year is divisible by 4, but not by 100 unless it is also divisible by 400.
+#Use an if-else statement to make this determination.
+#Input = 2024
+#Output = Leap year
+
+year = int(input("Enter the year : "))
+if (year % 4 == 0) or (year % 400 == 0 and year % 100 !=0):
+ print("Given year is a leap year")
+else:
+ print("Given year is not a leap year")
