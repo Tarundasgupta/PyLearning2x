cda88342b4d6844e5ee4882e1dea6c0ff39b13ee
Tarundasgupta <158543224+Tarundasgupta@users.noreply.github.com>
2024-02-25 23:10:36 +0530

    Update Assingment.py

--- 02_Feb2024/Assingment.py
+++ 02_Feb2024/Assingment.py
@@ -1,8 +1,18 @@
 # Take a user input (name, age, roll_number, phone_number) and print the user details
 name = input('Enter your name ')
 age = input('Please enter your age ')
-roll_number = input('Please enter you roll_number ')
+roll_number = input('Please enter your roll_number ')
 phone_number = input('Please enter your phone_number ')
 
 
 print('User Details:', '\nName:',name, '\nRoll_Number:',roll_number, '\nPhone_Number:',phone_number, '\nAge:',age)
+
+#O/P: Enter your name TDG
+#     Please enter your age 12
+#     Please enter your roll_number 23
+#     Please enter your phone_number 147852390
+#     User Details: 
+#     Name: TDG 
+#     Roll_Number: 23 
+#     Phone_Number: 147852390 
+#     Age: 12
